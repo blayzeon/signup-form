ee9b2e66d3b2667e8f398d395ecb141fb43d0ef6
blayzeon <blayzeon@gmail.com>
2022-05-14 13:41:28 -0400

    add form validation

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const form = document.querySelector('form');
 const pw1 = form.querySelector('#password');
 const pw2 = form.querySelector('#confirm');
-const pwError = form.querySelector('#pw-error');
 
+// toggles password error visuals on/off
 function toggleError(set="off"){
+    const pwError = form.querySelector('#pw-error');
     if (set === "on") {
         pw1.classList.add('invalid');
         pw2.classList.add('invalid');
@@ -19,12 +20,22 @@ pw1.addEventListener('change', toggleError);
 pw2.addEventListener('change', toggleError);
 
 form.addEventListener('submit', (e) => {
-    const invalid = document.querySelector(':invalid');
-
+    // ensures that the passwords match
     if (pw1.value !== pw2.value) {
         toggleError("on");
         e.preventDefault();
     }
 
-    e.preventDefault();
+    // prevent empty fields from being submitted
+    form.querySelectorAll('input').forEach((field) => {
+        if (field.value === "") {
+            field.setAttribute('required', "required");
+        }
+    });
+
+    const invalid = document.querySelector(':invalid');
+
+    if (invalid === true) {
+        e.preventDefault();
+    }
 });
